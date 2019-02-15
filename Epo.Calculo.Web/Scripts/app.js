755b2aae779f124513209a1ce355474dfbc24f68
Flavio Henrique <micromassi.flavio@tokiomarine.com.br>
2019-02-15 10:05:23 -0200

    Adicionado exemplo angular

---
+++ Epo.Calculo.Web/Scripts/app.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module('myApp', []);
+app.controller('customersCtrl', function ($scope, $http) {
+    $http.get("https://copadosfilmes.azurewebsites.net/api/filmes")
+  .then(function (response) { $scope.filmes = response.data; });
+});
